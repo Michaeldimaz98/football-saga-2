49a9929f0faca61fa63c93e8d16ebeaefc164c6f
Michaeldimaz98 <43607316+Michaeldimaz98@users.noreply.github.com>
2026-03-21 02:12:53 +0700

    feat: add clubPlayers database with all Indonesian clubs

---
+++ src/data/clubPlayers.js
@@ -0,0 +1,149 @@
+// Club NPC Players Database - Complete Indonesian Clubs
+const clubPlayers = {
+  persija: {
+    name: "Persija Jakarta",
+    logo: "⚪",
+    city: "Jakarta",
+    players: [
+      { name: "Andritany", role: "GK", power: 75, pace: 65, defense: 75, mentality: 78 },
+      { name: "Ondrej Kudela", role: "CB", power: 78, pace: 70, defense: 85, mentality: 80 },
+      { name: "Rizky Ridho", role: "CB", power: 76, pace: 72, defense: 82, mentality: 75 },
+      { name: "Firza Andika", role: "LB", power: 72, pace: 78, defense: 75, mentality: 70 },
+      { name: "Rio Fahmi", role: "RB", power: 70, pace: 75, defense: 72, mentality: 68 },
+      { name: "Hanif Sjahbandi", role: "DM", power: 73, pace: 72, defense: 78, mentality: 75 },
+      { name: "Maciej Gajos", role: "AM", power: 75, pace: 75, passing: 82, mentality: 78 },
+      { name: "Ryo Matsumura", role: "LW", power: 76, pace: 88, shooting: 80, stamina: 85 },
+      { name: "Witan Sulaeman", role: "RW", power: 74, pace: 85, shooting: 78, stamina: 82 },
+      { name: "Marko Simic", role: "ST", power: 75, pace: 80, shooting: 85, stamina: 78 },
+      { name: "Rayhan Hannan", role: "CM", power: 68, pace: 70, passing: 75, mentality: 70 }
+    ]
+  },
+  persib: {
+    name: "Persib Bandung",
+    logo: "🔵",
+    city: "Bandung",
+    players: [
+      { name: "Kevin Ray Mendoza", role: "GK", power: 76, pace: 65, defense: 80, mentality: 80 },
+      { name: "Nick Kuipers", role: "CB", power: 77, pace: 70, defense: 85, mentality: 82 },
+      { name: "Alberto Rodriguez", role: "CB", power: 75, pace: 68, defense: 82, mentality: 78 },
+      { name: "Rezaldi Hehanussa", role: "LB", power: 71, pace: 76, defense: 73, mentality: 70 },
+      { name: "Henhen Herdiana", role: "RB", power: 70, pace: 74, defense: 71, mentality: 68 },
+      { name: "Marc Klok", role: "CM", power: 78, pace: 72, passing: 85, mentality: 80 },
+      { name: "Dedi Kusnandar", role: "DM", power: 72, pace: 70, defense: 80, mentality: 74 },
+      { name: "Stefano Beltrame", role: "AM", power: 75, pace: 76, passing: 82, mentality: 77 },
+      { name: "Ciro Alves", role: "LW", power: 79, pace: 90, shooting: 85, stamina: 88 },
+      { name: "Febri Hariyadi", role: "RW", power: 71, pace: 82, shooting: 75, stamina: 80 },
+      { name: "David da Silva", role: "ST", power: 80, pace: 85, shooting: 88, stamina: 85 }
+    ]
+  },
+  persebaya: {
+    name: "Persebaya Surabaya",
+    logo: "🟢",
+    city: "Surabaya",
+    players: [
+      { name: "Ernando Ari", role: "GK", power: 75, pace: 65, defense: 78, mentality: 77 },
+      { name: "Dusan Stevanovic", role: "CB", power: 74, pace: 68, defense: 80, mentality: 76 },
+      { name: "Yan Victor", role: "CB", power: 73, pace: 66, defense: 78, mentality: 74 },
+      { name: "Reva Adi Utama", role: "LB", power: 70, pace: 74, defense: 71, mentality: 68 },
+      { name: "Arief Catur", role: "RB", power: 69, pace: 72, defense: 70, mentality: 66 },
+      { name: "Andre Oktaviansyah", role: "DM", power: 70, pace: 68, defense: 76, mentality: 70 },
+      { name: "M. Hidayat", role: "CM", power: 71, pace: 70, passing: 78, mentality: 72 },
+      { name: "Song Ui-young", role: "AM", power: 74, pace: 74, passing: 80, mentality: 75 },
+      { name: "Bruno Moreira", role: "LW", power: 77, pace: 86, shooting: 82, stamina: 84 },
+      { name: "Robson Duarte", role: "RW", power: 75, pace: 84, shooting: 80, stamina: 82 },
+      { name: "Wildan Ramdhani", role: "ST", power: 70, pace: 78, shooting: 76, stamina: 78 }
+    ]
+  },
+  psm: {
+    name: "PSM Makassar",
+    logo: "⛰️",
+    city: "Makassar",
+    players: [
+      { name: "Adin Aten", role: "GK", power: 73, pace: 62, defense: 76, mentality: 75 },
+      { name: "Yusran", role: "CB", power: 72, pace: 66, defense: 78, mentality: 74 },
+      { name: "Benny Wahyudi", role: "CB", power: 71, pace: 65, defense: 77, mentality: 72 },
+      { name: "Zulkifli", role: "LB", power: 69, pace: 72, defense: 70, mentality: 67 },
+      { name: "Muhammad Tahir", role: "RB", power: 68, pace: 70, defense: 69, mentality: 65 },
+      { name: "Sutrisno", role: "CM", power: 70, pace: 68, passing: 76, mentality: 71 },
+      { name: "Syahril", role: "CM", power: 69, pace: 67, passing: 75, mentality: 70 },
+      { name: "Riko Simanjuntak", role: "LW", power: 75, pace: 85, shooting: 80, stamina: 82 },
+      { name: "Willian Oliveira", role: "ST", power: 78, pace: 82, shooting: 85, stamina: 83 },
+      { name: "Sada Rahim", role: "RW", power: 72, pace: 80, shooting: 77, stamina: 79 },
+      { name: "Tedi Hermayadi", role: "AM", power: 71, pace: 72, passing: 78, mentality: 73 }
+    ]
+  },
+  bhayangkara: {
+    name: "Bhayangkara FC",
+    logo: "🚓",
+    city: "Jakarta",
+    players: [
+      { name: "Agus Sentosa", role: "GK", power: 72, pace: 60, defense: 75, mentality: 74 },
+      { name: "Suryanto", role: "CB", power: 70, pace: 64, defense: 76, mentality: 72 },
+      { name: "Arfan Dipo", role: "CB", power: 69, pace: 63, defense: 75, mentality: 71 },
+      { name: "Firman Utina", role: "LB", power: 68, pace: 70, defense: 69, mentality: 66 },
+      { name: "Wahyu Wirawan", role: "RB", power: 67, pace: 68, defense: 68, mentality: 64 },
+      { name: "Rizky Aditama", role: "CM", power: 69, pace: 67, passing: 74, mentality: 70 },
+      { name: "Ady Aryanto", role: "CM", power: 68, pace: 66, passing: 73, mentality: 69 },
+      { name: "Yunus Ginting", role: "LW", power: 72, pace: 82, shooting: 76, stamina: 80 },
+      { name: "Arfan Siahaan", role: "ST", power: 74, pace: 78, shooting: 80, stamina: 79 },
+      { name: "Taufik Rahman", role: "RW", power: 70, pace: 78, shooting: 74, stamina: 76 },
+      { name: "Makan Konate", role: "AM", power: 71, pace: 70, passing: 77, mentality: 72 }
+    ]
+  },
+  bali_united: {
+    name: "Bali United",
+    logo: "🏝️",
+    city: "Bali",
+    players: [
+      { name: "Stephane Ruffier", role: "GK", power: 77, pace: 65, defense: 82, mentality: 81 },
+      { name: "Ferreira", role: "CB", power: 75, pace: 68, defense: 81, mentality: 77 },
+      { name: "Agus Wardhana", role: "CB", power: 72, pace: 66, defense: 77, mentality: 73 },
+      { name: "Seto Setiawan", role: "LB", power: 70, pace: 74, defense: 71, mentality: 69 },
+      { name: "Ananda Basri", role: "RB", power: 69, pace: 72, defense: 70, mentality: 67 },
+      { name: "Cristian Gonzales", role: "CM", power: 74, pace: 70, passing: 80, mentality: 75 },
+      { name: "Rizky Dwi Putra", role: "CM", power: 70, pace: 68, passing: 76, mentality: 71 },
+      { name: "Otavio", role: "LW", power: 76, pace: 86, shooting: 81, stamina: 84 },
+      { name: "Hariono", role: "ST", power: 76, pace: 80, shooting: 83, stamina: 81 },
+      { name: "Welker", role: "RW", power: 74, pace: 84, shooting: 79, stamina: 82 },
+      { name: "Melvin Platje", role: "AM", power: 73, pace: 71, passing: 79, mentality: 74 }
+    ]
+  },
+  arema: {
+    name: "Arema FC",
+    logo: "🦅",
+    city: "Malang",
+    players: [
+      { name: "Eka Ramdani", role: "GK", power: 74, pace: 63, defense: 77, mentality: 76 },
+      { name: "Hamka Hamzah", role: "CB", power: 73, pace: 66, defense: 79, mentality: 75 },
+      { name: "Hilton Moreira", role: "CB", power: 76, pace: 68, defense: 82, mentality: 78 },
+      { name: "Putra Wiranatya", role: "LB", power: 70, pace: 74, defense: 72, mentality: 70 },
+      { name: "Franko Andrian", role: "RB", power: 69, pace: 72, defense: 71, mentality: 68 },
+      { name: "Cahyo Hasanudin", role: "CM", power: 72, pace: 70, passing: 78, mentality: 73 },
+      { name: "Dendi Santoso", role: "DM", power: 71, pace: 68, defense: 77, mentality: 72 },
+      { name: "Rizky Pellu", role: "LW", power: 74, pace: 84, shooting: 79, stamina: 81 },
+      { name: "Slavko Damjanovic", role: "ST", power: 77, pace: 80, shooting: 84, stamina: 82 },
+      { name: "Evan Dimas", role: "RW", power: 73, pace: 82, shooting: 78, stamina: 80 },
+      { name: "Petrovic", role: "AM", power: 75, pace: 72, passing: 81, mentality: 76 }
+    ]
+  },
+  madura_united: {
+    name: "Madura United",
+    logo: "🐄",
+    city: "Madura",
+    players: [
+      { name: "Afandi", role: "GK", power: 71, pace: 61, defense: 74, mentality: 73 },
+      { name: "Bambang Suryanto", role: "CB", power: 70, pace: 64, defense: 76, mentality: 72 },
+      { name: "Syaiful", role: "CB", power: 69, pace: 63, defense: 75, mentality: 71 },
+      { name: "Hendro Siswanto", role: "LB", power: 68, pace: 72, defense: 70, mentality: 67 },
+      { name: "Ahmad Jufriyanto", role: "RB", power: 67, pace: 70, defense: 69, mentality: 65 },
+      { name: "Sartono", role: "CM", power: 68, pace: 66, passing: 74, mentality: 69 },
+      { name: "Zulkarnain", role: "CM", power: 67, pace: 65, passing: 73, mentality: 68 },
+      { name: "Awan Setiawan", role: "LW", power: 71, pace: 80, shooting: 75, stamina: 78 },
+      { name: "Brahmantya", role: "ST", power: 72, pace: 76, shooting: 78, stamina: 77 },
+      { name: "Teguh Santoso", role: "RW", power: 69, pace: 77, shooting: 72, stamina: 75 },
+      { name: "Rendi Irawan", role: "AM", power: 68, pace: 68, passing: 75, mentality: 69 }
+    ]
+  }
+};
+
+module.exports = clubPlayers;
